b3fce21c383097cee52f76d5be1f40cdea3f6f27
BelenSezRami <belensezrami@gmail.com>
2023-10-23 18:36:25 +0200

    user model modified, bcrypt installed, and painting routes sorted

--- models/User.model.js
+++ models/User.model.js
@@ -4,14 +4,29 @@ const userSchema = new Schema(
   {
     email: {
       type: String,
-      required: [true, 'Email is required.'],
+      required: [true, 'El email es obligatorio.'],
       unique: true,
       lowercase: true,
       trim: true
     },
     password: {
       type: String,
-      required: [true, 'Password is required.']
+      required: [true, 'La contraseña es obligatoria.'],
+      minlength: [5, 'La contraseña tiene que tener al menos 5 caracteres.'],
+    },
+    name: {
+      type: String,
+      required: [true, 'El nombre es obligatorio']
+    },
+    image: {
+      type: String,
+      default: 'https://cdn.icon-icons.com/icons2/3066/PNG/512/user_person_profile_avatar_icon_190943.png',
+      required: [false]
+    },
+    role: {
+      type: String,
+      enum: ['USER', 'ADMIN'],
+      default: 'USER'
     }
   },
   {
