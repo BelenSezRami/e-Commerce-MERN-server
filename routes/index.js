917553b8b82e157aabd23bd30d3bf7b318f4dee5
BelenSezRami <belensezrami@gmail.com>
2023-11-03 12:46:18 +0100

    implemented cloudinary

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const router = require("express").Router()
 
 router.use("/paintings", require('./painting.routes'))
 router.use("/auth", require('./auth.routes'))
+router.use("/upload", require('./upload.routes'))
 
 module.exports = router
---
+++ routes/upload.routes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router()
+
+const uploaderMiddleware = require("../middlewares/uploader.middleware")
+
+router.post('/image', uploaderMiddleware.single('imageData'), (req, res) => {
+
+    if (!req.file) {
+        res.status(500).json({ errorMessage: 'Error caragndo el archivo' })
+        return
+    }
+
+    res.json({ cloudinary_url: req.file.path })
+})
+
+// router.post('/images', uploaderMiddleware.array('imagesData'), (req, res) => {
+
+//     if (!req.files || req.files.length === 0) {
+//         res.status(500).json({ errorMessage: 'Error caragndo archivos' })
+//         return
+//     }
+
+//     const urls = req.files.map(file => file.path)
+
+//     res.json({ cloudinary_urls: urls })
+// })
+
+
+module.exports = router
